4fb9e18d817a54c1a93b6406d75883a48d9280f3
村中　勇輝 <umeneri0725@gmail.com>
2018-08-25 14:51:34 +0900

    Initial Commit

---
+++ jibun-dictionary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jibun-dictionary
+//
+//  Created by 村中　勇輝 on 2018/08/25.
+//  Copyright © 2018年 村中　勇輝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
